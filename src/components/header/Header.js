72c7e179020ec05a67ce071f5771b9f769d03744
Your Name <your-email@example.com>
2025-07-04 21:16:48 +0530

    Fix mobile navbar and ensure all content is visible below navbar on all sections

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,53 +6,92 @@ import { NavLink } from "react-router-dom";
 import SeoHeader from "../seoHeader/SeoHeader";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuOpen: false,
+    };
+  }
+
+  toggleMenu = () => {
+    this.setState((prevState) => ({ isMenuOpen: !prevState.isMenuOpen }));
+  };
+
+  closeMenu = () => {
+    this.setState({ isMenuOpen: false });
+  };
+
   render() {
+    const { isMenuOpen } = this.state;
     return (
       <Fade top duration={1000} distance="20px">
         <SeoHeader />
-        <div>
-          <header className="header custom-navbar">
-            {/* Hamburger menu for small screens */}
-            <input className="menu-btn" type="checkbox" id="menu-btn" />
-            <label className="menu-icon" htmlFor="menu-btn">
-              <span className="navicon"></span>
-            </label>
-            <nav className="navbar-menu">
-              <ul className="menu">
-                <li>
-                  <NavLink exact to="/home" activeClassName="active-link">
-                    Home
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/education" activeClassName="active-link">
-                    Education
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/experience" activeClassName="active-link">
-                    Experience
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/projects" activeClassName="active-link">
-                    Projects
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/contact" activeClassName="active-link">
-                    Contact Me
-                  </NavLink>
-                </li>
-                <li>
-                  <a href="#hire" className="hire-btn">
-                    Hire me
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </header>
-        </div>
+        <header className="header custom-navbar">
+          {/* Hamburger menu for small screens */}
+          <button
+            className="menu-icon"
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            aria-expanded={isMenuOpen}
+            onClick={this.toggleMenu}
+            style={{ background: "none", border: "none", cursor: "pointer" }}
+          >
+            <span className="navicon"></span>
+            {/* Optionally add an 'X' or hamburger icon here based on isMenuOpen */}
+          </button>
+          <ul className={`menu${isMenuOpen ? " open" : ""}`}>
+            <li>
+              <NavLink
+                exact
+                to="/home"
+                activeClassName="active-link"
+                onClick={this.closeMenu}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/education"
+                activeClassName="active-link"
+                onClick={this.closeMenu}
+              >
+                Education
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/experience"
+                activeClassName="active-link"
+                onClick={this.closeMenu}
+              >
+                Experience
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/projects"
+                activeClassName="active-link"
+                onClick={this.closeMenu}
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                activeClassName="active-link"
+                onClick={this.closeMenu}
+              >
+                Contact Me
+              </NavLink>
+            </li>
+            <li>
+              <a href="#hire" className="hire-btn" onClick={this.closeMenu}>
+                Hire me
+              </a>
+            </li>
+          </ul>
+        </header>
       </Fade>
     );
   }
